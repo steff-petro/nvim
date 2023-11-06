01e44ea0abe86f7f90860ae31ea9bf689adf929e
steff-petro <stefanp0618@gmail.com>
2023-11-06 22:31:26 +0100

    first commit with all files

---
+++ lua/core/plugin_config/init.lua
@@ -0,0 +1,5 @@
+require("core.plugin_config.lualine")
+require("core.plugin_config.nvim-tree")
+require("core.plugin_config.gruvbox")
+require("core.plugin_config.keymaps")
+require("core.plugin_config.options")
